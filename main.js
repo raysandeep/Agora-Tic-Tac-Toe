36b09f8d5899a4767adcd90e75faba22b17a59f6
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-04-14 17:46:02 +0530

    Added Base Code for Webinar

--- main.js
+++ main.js
@@ -4,32 +4,6 @@ var N_SIZE = 3,
     turn = "X",
     score,
     moves;
-var myTurn = "X"
-var isOpponentsTurn = false;
-
-// Constants
-var agoraAppId = "a6af85f840ef43108491705e2315a857";
-var isLoggedIn = false;
-var username = null;
-var channel;
-var users;
-
-var username = prompt("Enter your username", "raysandeep")
-if (username == null) {
-    alert("Enter Name!");
-    // window.location.reload();
-}
-var channelName = prompt("Enter your room name", "room")
-if (channelName == null) {
-    alert("Enter Room Name!");
-    // window.location.reload();
-}
-
-
-function startStuff() {
-    startAgoraRTM(username, channelName);
-    document.getElementById('turn').innerHTML = 'Waiting for player to join';
-}
 
 function init() {
     var board = document.createElement('table');
@@ -46,7 +20,7 @@ function init() {
             cell.setAttribute('width', 120);
             cell.setAttribute('align', 'center');
             cell.setAttribute('valign', 'center');
-            cell.setAttribute('id', j.toString() + i.toString());
+            cell.setAttribute('id', i.toString() + j.toString());
             cell.classList.add('col' + j, 'row' + i);
             if (i == j) {
                 cell.classList.add('diagonal0');
@@ -64,11 +38,6 @@ function init() {
 
     document.getElementById("tictactoe").appendChild(board);
     startNewGame();
-    if (myTurn == 'X') {
-        document.getElementById('turn').textContent = 'Your turn';
-    } else {
-        document.getElementById('turn').textContent = 'Opponent turn';
-    }
 }
 
 /*
@@ -110,97 +79,18 @@ function contains(selector, text) {
     });
 }
 
-
-
-
-
-
-
-// $("#sendMsgBtn").prop("disabled", true);
-
-// RtmClient
-const client = AgoraRTM.createInstance(agoraAppId, { enableLogUpload: false });
-
-function startAgoraRTM(accountName, channelName) {
-
-    // Login
-    client.login({ uid: accountName }).then(() => {
-        console.log('AgoraRTM client login success. Username: ' + accountName);
-        isLoggedIn = true;
-        channel = client.createChannel(channelName);
-        // document.getElementById("channelNameBox").innerHTML = channelName;
-        channel.join().then(() => {
-            // Receive Channel Message
-            channel.on('ChannelMessage', ({ text }, senderId) => {
-                movement_data = JSON.parse(text);
-                console.log(movement_data)
-                if (movement_data.message == "Movement") {
-                    console.log("Opponent clicked on" + movement_data.data.location)
-                    console.log(document.getElementById(movement_data.data.location))
-                    setOpponent(document.getElementById(movement_data.data.location))
-                } else if (movement_data.message == "Win") {
-                    alert('Opponent Won!');
-                    startNewGame()
-                }
-
-            });
-            channel.getMembers().then((message) => {
-                users = message
-                console.log(users)
-                if (users.length == 2) {
-                    if (users[0] != username)
-                        document.getElementById('turn').innerHTML = users[0] + " had joined the room"
-                    else
-                        document.getElementById('turn').innerHTML = users[1] + " had joined the room"
-
-                    init()
-
-                }
-            })
-            channel.on('MemberJoined', memberId => {
-                myTurn = "O"
-                isOpponentsTurn = true
-                document.getElementById('turn').innerHTML = memberId + " had joined the room"
-                init()
-            })
-            channel.on('MemberLeft', memberId => {
-                alert("Opponent left")
-                window.location.reload();
-            });
-        }).catch(error => {
-            console.log('AgoraRTM client channel join failed: ', error);
-        }).catch(err => {
-            console.log('AgoraRTM client login failure: ', err);
-        });
-    });
-}
-
-
 /*
 * Sets clicked square and also updates the turn.
 */
 function set() {
-    console.log(this.innerHTML, turn, myTurn, isOpponentsTurn,)
     if (this.innerHTML !== EMPTY) {
-        return;
-    } else if (myTurn == turn && isOpponentsTurn == true) {
-        return;
-    } else if (myTurn != turn) {
         return;
     }
     this.innerHTML = turn;
-    var moves_loc = this.id;
     moves += 1;
     score[turn] += this.identifier;
-    console.log(score)
-    console.log(turn)
     if (win(this)) {
-        channel.sendMessage({ text: JSON.stringify({ message: 'Win', data: turn + " won!" }) }).then(() => {
-            console.log("Message sent!");
-        }).catch(error => {
-            console.log("Message wasn't sent due to an error: ", error);
-        });
-        alert('You Won!');
+        alert('Winner: Player ' + turn);
         startNewGame();
     } else if (moves === N_SIZE * N_SIZE) {
         alert("Draw");
@@ -208,52 +98,7 @@ function set() {
     } else {
         turn = turn === "X" ? "O" : "X";
         document.getElementById('turn').textContent = 'Player ' + turn;
-        channel.sendMessage({ text: JSON.stringify({ message: 'Movement', data: { moves: moves, location: moves_loc } }) }).then(() => {
-            console.log("Message sent!");
-        }).catch(error => {
-            console.log("Message wasn't sent due to an error: ", error);
-        });
-        document.getElementById('turn').textContent = 'Opponent\'s turn';
     }
 }
 
-
-function setOpponent(object) {
-    console.log(object.innerHTML, turn, myTurn, isOpponentsTurn,)
-    if (object.innerHTML !== EMPTY) {
-        return;
-    } else if (myTurn == turn && isOpponentsTurn == true) {
-        return;
-    }
-    object.innerHTML = turn;
-    moves += 1;
-    score[turn] += object.identifier;
-    console.log(score)
-    console.log(turn)
-    if (win(object)) {
-        alert('Opponent Won!');
-        startNewGame();
-    } else if (moves === N_SIZE * N_SIZE) {
-        alert("Draw");
-        startNewGame();
-    } else {
-        turn = turn === "X" ? "O" : "X";
-        document.getElementById('turn').textContent = 'Your turn';
-    }
-    isOpponentsTurn = false;
-}
-
-
-
-
-
-// Logout
-function leaveChannel() {
-    channel.leave();
-    client.logout();
-    isLoggedIn = false;
-    $("#joinChannelBtn").prop("disabled", false);
-    $("#sendMsgBtn").prop("disabled", true);
-    $("#joinChannelModal").modal('open');
-    console.log("Channel left successfully and user has been logged out.");
-}
+init();
